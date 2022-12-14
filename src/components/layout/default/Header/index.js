13ed37b7296b989a31af353d678984efcc5dab37
tandat <tandatsm@gmail.com>
2022-12-14 16:05:20 +0700

    add user login on header

--- src/components/layout/default/Header/index.js
+++ src/components/layout/default/Header/index.js
@@ -3,9 +3,17 @@ import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 
+// icons
+import {FaUser} from 'react-icons/fa';
+//
 import { PulicRoutes } from '../../../../routes/Route';
+import { useEffect } from 'react';
+//import { useNavigate } from 'react-router-dom';
+
+
 
 const textNonUnderline = { 
     color: 'inherit', 
@@ -13,6 +21,18 @@ const textNonUnderline = {
 }
 
 export default function Header() {
+   // let navigate = useNavigate();
+    const [user, setUser] = useState("");
+      useEffect(() => {
+     var user = localStorage.getItem('username');
+    setUser(user);
+    },[]);
+const logOut = () =>{
+    localStorage.removeItem('email');
+    localStorage.removeItem('username');
+    localStorage.clear();
+    window.location.reload();
+}
     return (
         <>
             <Navbar expand="lg" className='p-3' style={{boxShadow: '0px 0px 20px 1px lightgrey'}}>
@@ -39,6 +59,15 @@ export default function Header() {
                     <Link style={textNonUnderline} to="/contact">Contact</Link>
                 </Nav.Link>
                 <Button variant="primary">Make an Appoinment</Button>
+                <Nav.Link>
+                    <Button variant='white'>
+                        <div className='btn btn-light'>
+                        <FaUser></FaUser>
+                       <p>{user}</p> 
+                        </div>
+                        </Button>
+                        <Link to='/login'>Login</Link> / <Link to='' onClick={logOut}>Logout</Link>
+                </Nav.Link>
             </Container>
             </Navbar>
 
