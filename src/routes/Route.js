86acd0c2623260e0011d18d7c955b8cc63fff761
DatHuynhbk <114873093+DatHuynhbk@users.noreply.github.com>
2022-12-14 21:36:01 +0700

    Revert "merge code lai nha"

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,10 +1,5 @@
 import Home from "../pages/Home"
 import Contact from "../pages/Contact"
-import Department from "../pages/Department"
-import Blog from "../pages/Blog"
-import Login from "../pages/Login"
-import Register from "../pages/Register"
-import Create from "../pages/create"
 
 export const PulicRoutes = [
     {
@@ -14,27 +9,7 @@ export const PulicRoutes = [
     {
         path: '/contact',
         element: Contact
-    },
-    {
-        path: '/department',
-        element: Department
-    },
-    {
-        path: '/blog',
-        element: Blog
-    },
-    {
-        path: '/login',
-        element: Login
-    },
-    {
-        path: '/register',
-        element: Register
-    },
-{
-    path: '/create',
-    element: Create
-}
+    }
 ]
 
 export const PrivateRoutes = [
